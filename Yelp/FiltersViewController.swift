308d5584182e9a8ab531cd1f08e3e51f6edbb876
Hugo Nguyen <hugo@scribd.com>
2016-02-11 00:21:19 -0800

    basic filter functionality

--- Yelp/FiltersViewController.swift
+++ Yelp/FiltersViewController.swift
@@ -8,14 +8,48 @@
 
 import UIKit
 
-class FiltersViewController: UIViewController {
+@objc protocol FiltersViewControllerDelegate {
+    optional func filtersViewController(filtersViewController: FiltersViewController,
+        didUpdateFilters filters: [String: AnyObject])
+}
+
+class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, SwitchCellDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    weak var delegate: FiltersViewControllerDelegate?
+
+    var categories: [[String:String]]!
+    var switchStates = [Int:Bool]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        categories = yelpCategories()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("SwitchCell", forIndexPath: indexPath) as! SwitchCell
+        
+        cell.switchLabel.text = categories[indexPath.row]["name"]
+        cell.delegate = self
+        cell.onSwitch.on = switchStates[indexPath.row] ?? false
+        
+        return cell
+    }
+    
+    func switchCell(switchCell: SwitchCell, didChangeValue value: Bool) {
+        let indexPath = tableView.indexPathForCell(switchCell)!
+        
+        switchStates[indexPath.row] = value
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,16 +62,27 @@ class FiltersViewController: UIViewController {
     }
 
     @IBAction func onSearch(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        var filters = [String: AnyObject]()
+        var selectedCategories = [String]()
+        for (row, isSelected) in switchStates {
+            if isSelected {
+                selectedCategories.append(categories[row]["code"]!)
+            }
+        }
+        
+        if selectedCategories.count > 0 {
+            filters["categories"] = selectedCategories
+        }
+        
+        delegate?.filtersViewController?(self, didUpdateFilters: filters)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func yelpCategories() -> [[String:String]] {
+        return [["name": "Afghan", "code": "afghani"],
+        ["name": "African", "code": "african"],
+        ["name": "American, New", "code": "newamerican"]]
     }
-    */
 
 }
--- Yelp/SwitchCell.swift
+++ Yelp/SwitchCell.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
+@objc protocol SwitchCellDelegate {
+    optional func switchCell(switchCell: SwitchCell,
+        didChangeValue value: Bool)
+}
+
 class SwitchCell: UITableViewCell {
 
     @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var onSwitch: UISwitch!
     
+    weak var delegate: SwitchCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        onSwitch.addTarget(self, action: "switchValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func switchValueChanged(mySwitch: UISwitch) {
+        delegate?.switchCell?(self, didChangeValue: mySwitch.on)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
