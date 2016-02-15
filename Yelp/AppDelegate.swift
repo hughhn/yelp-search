d6d71792c48c9f6922c8c77aef9e02bd39ae4dde
Hugh Nguyen <hugh.hn@gmail.com>
2016-02-14 20:38:32 -0800

    custom navigation bar

--- Yelp/AppDelegate.swift
+++ Yelp/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 196.0/255.0, green: 18.0/255.0, blue: 0.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        
         return true
     }
 
